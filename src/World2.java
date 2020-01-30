b08d4041e38f5a150f2c1c11444660a9715b5573
Noah Triche <noahthetriche@Noahs-Air.home>
2020-01-29 17:00:21 -0800

    Initial commit

---
+++ src/World2.java
@@ -0,0 +1,10 @@
+public class World2
+{
+    public static void main(String []args)
+    {
+        Dictionary2 webster = new Dictionary2(1500, 52500);
+        webster.pageMessage();
+        webster.definitionMessage();
+    }
+}
+//noah triche per 5
